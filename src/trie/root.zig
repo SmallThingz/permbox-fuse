50626156021578ce5cd205469b6aa716e4093b99
ItsMeSamey <sameychain5041@gmail.com>
2026-07-17 04:05:15 +1000

    slightly less broken del

--- src/trie/root.zig
+++ src/trie/root.zig
@@ -16,31 +16,32 @@ pub fn add(_path: []const u8, data: Mode) !void {
     var index_at_buf: [3]u8 = undefined;
     std.mem.writeInt(u24, &index_at_buf, @intCast(b.root), native_endian);
     var index_at: Node.IndexAt = .{ .at = 0, .arr = @ptrCast(&index_at_buf) };
-    var node = Node.fromIdx(index_at.get()) catch unreachable;
+    defer b.root = std.mem.readInt(u24, &index_at_buf, native_endian);
+    var node = index_at.getNode() catch unreachable;
 
     blk: switch (node.next(path)) {
-        .node => |idx| { // Go to next node
-            path = path[node.radix_len + 1 ..];
-            std.debug.assert(path.len != 0);
-            index_at = node.indexAt(idx);
-            node = try Node.fromIdx(index_at.get());
-            continue :blk node.next(path);
-        },
         .this => |idx| { // the entry already exists; only need to set data
             if (node.bitset.isSet(idx)) {
-                const next = try Node.fromIdx(node.indexAt(idx).get());
+                const next = try node.indexAt(idx).getNode();
                 next.data = data;
             } else {
                 node.indexAt(idx).set(data);
             }
+        },
+        .node => |idx| { // Go to next node
+            path = path[node.radix_len + 1 ..];
+            std.debug.assert(path.len != 0);
+            index_at = node.indexAt(idx);
+            node = try index_at.getNode();
+            continue :blk node.next(path);
         },
         .diff => |idx| {
             const ogpath = path;
             path = path[idx + 1 ..];
 
             const left_idx: u24 = init: {
-                const new_idx = try Node.acquire();
-                const new = Node.fromIdx(new_idx) catch unreachable;
+                const new_idx = try acquire();
+                const new = fromIdx(new_idx) catch unreachable;
                 new.radix_len = node.radix_len - idx - 1;
                 new.data = .midway;
                 @memcpy(new.radix_str[0..new.radix_len], node.radix_str[idx + 1 ..]);
@@ -48,13 +49,13 @@ pub fn add(_path: []const u8, data: Mode) !void {
                 @memcpy(std.mem.asBytes(new)[boff..], std.mem.asBytes(node)[boff..]);
                 break :init new_idx;
             };
-            errdefer Node.releaseOne(left_idx);
+            errdefer releaseOne(left_idx);
 
             const need_right = path.len != 0;
             const right_idx: u24 = init: {
                 if (!need_right) break :init 0;
-                const new_idx = try Node.acquire();
-                const new = Node.fromIdx(new_idx) catch unreachable;
+                const new_idx = try acquire();
+                const new = fromIdx(new_idx) catch unreachable;
                 var current = new;
                 while (true) {
                     current.radix_len = @min(path.len - 1, node.radix_str.len);
@@ -68,19 +69,19 @@ pub fn add(_path: []const u8, data: Mode) !void {
                         break :init new_idx;
                     }
 
-                    const new_next = try Node.acquire();
+                    const new_next = try acquire();
                     current.bitset.set(next_idx);
                     current.indexAt(next_idx).set(new_next);
-                    current = Node.fromIdx(new_next) catch unreachable;
+                    current = fromIdx(new_next) catch unreachable;
                 }
             };
             errdefer {
-                if (need_right) Node.release(right_idx, true);
+                if (need_right) releaseLine(right_idx, true);
             }
 
             const top_idx: u24 = init: {
-                const new_idx = try Node.acquire();
-                const new = Node.fromIdx(new_idx) catch unreachable;
+                const new_idx = try acquire();
+                const new = fromIdx(new_idx) catch unreachable;
                 new.radix_len = idx;
                 new.data = node.data;
                 @memcpy(new.radix_str[0..idx], node.radix_str[0..idx]);
@@ -96,10 +97,10 @@ pub fn add(_path: []const u8, data: Mode) !void {
 
                 break :init new_idx;
             };
-            errdefer Node.releaseOne(top_idx);
+            errdefer releaseOne(top_idx);
 
             const og = index_at.get();
-            defer Node.releaseOne(og);
+            defer releaseOne(og);
             Pool.global.sync();
 
             // -> CRITICAL SECTION
@@ -113,12 +114,12 @@ pub fn add(_path: []const u8, data: Mode) !void {
 pub fn get(_path: []const u8) !?Mode {
     var path = _path;
     if (path.len == 0) return null;
-    var node = try Node.fromIdx(Pool.global.block().root);
+    var node = try fromIdx(Pool.global.block().root);
 
     blk: switch (node.next(path)) {
         .this => |idx| {
             if (node.bitset.isSet(idx)) {
-                const sub = try Node.fromIdx(node.indexAt(idx).get());
+                const sub = try node.indexAt(idx).getNode();
                 return sub.data;
             } else {
                 const val = node.indexAt(idx).get();
@@ -129,7 +130,7 @@ pub fn get(_path: []const u8) !?Mode {
         .next => |idx| {
             if (!node.bitset.isSet(idx)) return null;
             path = path[node.radix_len + 1 ..];
-            node = try Node.fromIdx(node.indexAt(idx).get());
+            node = try node.indexAt(idx).getNode();
             continue :blk node.next(path);
         },
         .diff => {
@@ -145,30 +146,140 @@ pub fn del(_path: []const u8) !Mode {
     var index_at_buf: [3]u8 = undefined;
     std.mem.writeInt(u24, &index_at_buf, @intCast(b.root), native_endian);
     var index_at: Node.IndexAt = .{ .at = 0, .arr = @ptrCast(&index_at_buf) };
-    var node = Node.fromIdx(index_at.get()) catch unreachable;
+    defer b.root = std.mem.readInt(u24, &index_at_buf, native_endian);
+    var node = index_at.getNode() catch unreachable;
+    var right_node_idx: ?u8 = null;
 
     blk: switch (node.next(path)) {
         .this => |idx| {
-            const sub_idx = node.indexAt(idx).get();
-            if (node.bitset.isSet(idx)) {
-                const sub = try Node.fromIdx(sub_idx);
-                const old = sub.data;
-                sub.data = .midway;
-                return old;
-            } else {
-                node.indexAt(idx).set(0); // 0 is .midway
-                return @enumFromInt(@as(u8, @truncate(sub_idx)));
+            // 1. Delete the data
+            const old_data: Mode = init: {
+                if (node.bitset.isSet(idx)) {
+                    const sub_idx = node.indexAt(idx).get();
+                    const sub = try fromIdx(sub_idx);
+                    const old = sub.data;
+                    sub.data = .midway;
+                    break :init old;
+                } else {
+                    const val = node.indexAt(idx).get();
+                    node.indexAt(idx).set(0);
+                    break :init @enumFromInt(@as(u8, @truncate(val)));
+                }
+            };
+
+            if (old_data == .midway) return error.NotFound;
+
+            const top_node = try index_at.getNode();
+            const r_idx = right_node_idx orelse init: {
+                const ni = node.indexAt(idx);
+                const n = try ni.getNode();
+                if (!n.bitset.isSet(idx)) return old_data;
+                const valcnt = n.valcntbounded(2);
+                if (valcnt == 0) { // sus; maybe corrupted file
+                    @branchHint(.cold);
+                    const niv = ni.get();
+                    // -> CRITICAL SECTION
+                    node.bitset.unset(idx);
+                    ni.set(0);
+                    // <- CRITICAL SECTION
+                    try releaseOne(niv, false);
+                } else if (valcnt == 1) {
+                    break :init idx;
+                }
+                return old_data;
+            };
+
+            if (node.data != .midway or node.valcntbounded(1) == 1) return old_data;
+            // Because of our top_node rule, if `node` is empty, the ENTIRE chain
+            // from `right_node` down to `node` is empty and can be removed!
+
+            const right_idx = top_node.indexAt(r_idx).get();
+            // -> CRITICAL SECTION
+            top_node.bitset.unset(r_idx);
+            top_node.indexAt(r_idx).set(0);
+            // <- CRITICAL SECTION
+            releaseLine(right_idx, false);
+
+            if (top_node.data != .midway) return old_data;
+            const topvalcnt = top_node.valcntbounded(2);
+            std.debug.assert(topvalcnt != 0);
+            if (topvalcnt != 1) return old_data;
+            // If top node now has exactly 1 child and no data, we must merge it;
+
+            if (@intFromPtr(index_at.arr) == @intFromPtr(&index_at_buf)) { // index_at was root
+                @branchHint(.unlikely);
+                // TODO
+            } else { // Normal case
+                // TODO
             }
+
+            return old_data;
         },
         .next => |idx| {
-            if (!node.bitset.isSet(idx)) return error.NotFound;
+            if (!node.bitset.isSet(idx)) {
+                @branchHint(.cold);
+                return error.NotFound;
+            }
+
+            const valcnt = node.valcntbounded(2);
+            if (valcnt > 1 or (valcnt == 1 and node.data != .midway)) {
+                // Update top node (index_at) ONLY if current node is a fork or has data
+                index_at = node.indexAt(idx);
+                right_node_idx = null;
+            } else if (right_node_idx == null) {
+                // Node has 1 child and no data. Top node stays, right node is this child.
+                right_node_idx = idx;
+            }
+
             path = path[node.radix_len + 1 ..];
-            node = try Node.fromIdx(node.indexAt(idx).get());
-            continue :blk node.next(path);
+            node = try fromIdx(node.indexAt(idx).get());
+            continue :blk node.next();
         },
         .diff => {
-            return .midway;
+            @branchHint(.cold);
+            return error.NotFound;
         },
+    }
+}
+
+/// Acquire a brand new node from the pool and init it with the required values
+fn acquire() !u24 {
+    const node_idx = try Pool.global.acquire();
+    const node = fromIdx(node_idx) catch unreachable;
+    node.* = .{};
+    return node_idx;
+}
+
+/// Release the node to the pool; does Not release all the nodes; only this one
+fn releaseOne(self: u24) void {
+    Pool.global.release(self);
+}
+
+fn fromIdx(self: u24) Pool.OOB!*@This() {
+    return Pool.global.nodeAt(self);
+}
+
+/// Releases a whole line of nodes starting from `start_idx`.
+/// Assumes every node in the chain has exactly 1 child, except the last one which has 0.
+fn releaseLine(start_idx: u24, comptime assert_inbounds: bool) if (assert_inbounds) void else (Pool.OOB || PathTooLong)!void {
+    var curr_idx = start_idx;
+    while (true) {
+        const node = if (assert_inbounds) fromIdx(curr_idx) catch unreachable else try fromIdx(curr_idx);
+        const count = node.bitset.count();
+        if (count == 0) { // Reached the end of the line
+            @branchHint(.unlikely);
+            Pool.global.release(curr_idx);
+            break;
+        } else if (count > 1) { // If we encounter a fork in the "line", something went wrong.
+            @branchHint(.cold);
+            if (assert_inbounds) unreachable else return error.PathTooLong;
+        } else { // count == 1: find the single child, release current, and step down
+            @branchHint(.likely);
+            const child_byte = @as(u8, @intCast(node.bitset.findFirstSet().?));
+            const next_idx = node.indexAt(child_byte).get();
+            Pool.global.release(curr_idx);
+            curr_idx = next_idx;
+        }
     }
 }
 
@@ -196,80 +307,16 @@ pub const Node = extern struct {
             return std.mem.readInt(u24, &me.arr[me.at][0..3], native_endian);
         }
 
+        pub fn getNode(me: @This()) !*Node {
+            return fromIdx(me.get());
+        }
+
         pub fn set(me: @This(), val: u24) void {
             return std.mem.writeInt(u24, &me.arr[me.at][0..3], val, native_endian);
         }
     };
     pub fn indexAt(self: *@This(), at: u8) IndexAt {
         return .{ .arr = &self.idx_arr, .at = at * 3 };
-    }
-
-    fn fromIdx(self: u24) Pool.OOB!*@This() {
-        return Pool.global.nodeAt(self);
-    }
-
-    fn toIdx(self: *@This()) Pool.OOB!u24 {
-        return Pool.global.indexOf(self);
-    }
-
-    /// Acquire a brand new node from the pool and init it with the required values
-    fn acquire() !u24 {
-        const node_idx = try Pool.global.acquire();
-        const node = fromIdx(node_idx) catch unreachable;
-        node.* = .{};
-        return node_idx;
-    }
-
-    /// Release the node to the pool; does Not release all the nodes; only this one
-    fn releaseOne(self: u24) void {
-        self.* = undefined;
-        Pool.global.release(self);
-    }
-
-    fn release(idx: u24, comptime assert_inbounds: bool) if (assert_inbounds) void else (Pool.OOB || PathTooLong)!void {
-        var stack: [max_depth]BlkIdx = undefined;
-        stack[0] = .{ .chr = 0, .idx = idx };
-        var len = 1;
-        outer: while (len > 0) {
-            const top = &stack[len - 1];
-            const node = if (assert_inbounds) fromIdx(top.blk) catch unreachable else try fromIdx(top.blk);
-
-            if (len == stack.len) {
-                @branchHint(.cold);
-                for (top.chr..0xff) |i| {
-                    if (node.bitset.isSet(i)) {
-                        const subidx = node.indexAt(@intCast(i)).get();
-
-                        const subnode = if (assert_inbounds) fromIdx(subidx) catch unreachable else try fromIdx(subidx);
-                        inline for (0..4) |j| {
-                            if (subnode.bitset[j] != 0) {
-                                if (assert_inbounds) unreachable;
-                                return PathTooLong.PathTooLong;
-                            }
-                        }
-                        Pool.global.release(subidx);
-                    }
-                }
-            } else {
-                for (top.chr..0xff) |i| {
-                    if (node.bitset.isSet(i)) {
-                        top.chr = @bitCast(i);
-                        stack[len] = .{ .chr = 0, .blk = node.indexAt(@intCast(i)).get() };
-
-                        len += 1;
-                        continue :outer;
-                    }
-                }
-            }
-
-            const blk = top.blk;
-            if (node.bitset.isSet(0xff)) {
-                top.* = .{ .chr = 0, .blk = node.indexAt(0xff).get() };
-            } else {
-                len -= 1;
-            }
-            Pool.global.release(blk);
-        }
     }
 
     const Next = union(enum) {
@@ -287,6 +334,21 @@ pub const Node = extern struct {
         const consumed = maybe_consumed.?;
         if (consumed == self.radix_len) return .{ .next = path[self.radix_len] };
         return .{ .diff = consumed };
+    }
+
+    fn valcntbounded(self: *@This(), comptime less_than: comptime_int) u8 {
+        const bitsetcnt: u8 = self.bitset.count();
+        if (bitsetcnt >= less_than) return less_than;
+        for (0..children_count) |i| {
+            if (!self.bitset.isSet(i) and @as(u8, @truncate(self.indexAt(i).get())) != 0) {
+                bitsetcnt += 1;
+                if (bitsetcnt == less_than) {
+                    @branchHint(.unlikely);
+                    return less_than;
+                }
+            }
+        }
+        return bitsetcnt;
     }
 };
 
@@ -317,9 +379,9 @@ pub const Mode = packed struct(u8) {
     /// Controls weather exec is allowed. Disallowing this for a dir means the process can't read subdiles/subdirs
     x: A,
 
-    pub const midway: @This() = .{ .k = .midway, .r = undefined, .w = undefined, .x = undefined };
-    pub const dir: @This() = .{ .k = .visible, .value = true, .r = .allow, .w = .overlay, .x = .allow };
-    pub const file: @This() = .{ .k = .visible, .value = true, .r = .deny, .w = .overlay, .x = .allow };
+    pub const midway: @This() = @bitCast(0);
+    pub const dir: @This() = .{ .k = .visible, .r = .allow, .w = .overlay, .x = .allow };
+    pub const file: @This() = .{ .k = .visible, .r = .deny, .w = .overlay, .x = .allow };
 
     pub const K = enum(u2) {
         /// This is a mid-way node and does not have it's own data; may or may not have children
